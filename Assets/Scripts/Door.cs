184a60f8097efe9a67b25c4a608466c2500527dc
KreconyMakaron <KreconyMakaron@users.noreply.github.com>
2020-12-02 21:12:49 +0100

    Added stuff

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Door : MonoBehaviour
+{
+    public Transform door;
+    public Transform closepos;
+    public Transform openpos;
+
+    public bool open = false;
+    public float speed = 3f;
+
+    void Start()
+    {
+        door.transform.position = closepos.transform.position;
+    }
+
+    void Update()
+    {
+        if (open && Vector3.Distance(door.position, openpos.position) > 0.001f)
+        {
+            door.position = Vector3.MoveTowards(door.position, openpos.position, speed * Time.deltaTime);
+        }
+        if (!open && Vector3.Distance(door.position, closepos.position) > 0.001f)
+        {
+            door.position = Vector3.MoveTowards(door.position, closepos.position, speed * Time.deltaTime);
+        }
+    }
+
+    public void OpenClose()
+    {
+        open = !open;
+    }
+}
---
+++ Assets/Scripts/Lock.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    bool iCanOpen = false;
+    public Door[] doors;
+    public KeyColor myColor;
+    public bool locked = false;
+    Animator key;
+
+    private void Start()
+    {
+        key = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            iCanOpen = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            iCanOpen = false;
+        }
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void UseKey()
+    {
+        foreach (Door door in doors)
+        {
+            door.OpenClose();
+        }
+    }
+}
